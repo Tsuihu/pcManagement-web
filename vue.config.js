d5bfcc2c00c508642c24d2d76a1f035e51b0ed36
tsuihu <1335973535@qq.com>
2022-11-10 22:17:38 +0800

    完善

--- vue.config.js
+++ vue.config.js
@@ -17,7 +17,7 @@ module.exports = {
     proxy: {
       "/api": {
         //后端的访问地址
-        target: "http://127.0.0.1:8080/",
+        target: "http://127.0.0.1:5051",
         changeOrigin: true,
         pathRewrite: {
           "^/api": "/"
